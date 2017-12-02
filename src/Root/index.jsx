20aecb7831f9f51f6ae568435d29c97b6055ddd8
Антон Чащин <anton.casin@gmail.com>
2017-12-03 02:03:35 +0300

    wtf

--- src/Root/index.jsx
+++ src/Root/index.jsx
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import { Switch, Redirect, Route, Router } from 'react-router-dom';
+import { InlineMath } from 'react-katex';
 
 import { actions } from 'store';
 import Spinner from 'components/Spinner';
@@ -8,9 +9,13 @@ import Title from 'components/Title';
 import Button from 'components/Button';
 import Text from 'components/Text';
 import Clipboard from 'components/Clipboard';
+import RowInput from 'components/RowInput';
 import urls from 'config/urls';
 import history from 'utils/history';
+import core from './core';
+import { Term } from './core/utils';
 
+import './katex.scss';
 import styles from './styles.scss';
 
 
@@ -20,7 +25,16 @@ export default class extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            minterms: '',
+            minterms: '1, 2, 3',
+            variables: ['x_1', 'x_2', 'x_3', ],
+            FDNFForHuman: null,
+            FDNFForComputer: null,
+            SOPForHuman: null,
+            SOPForComputer: null,
+            SOPAllForHuman: null,
+            SOPAllForComputer: null,
+            SOPBestForHuman: null,
+            SOPBestForComputer: null,
         }
     }
 
@@ -29,16 +43,97 @@ export default class extends React.Component {
         this.setState({ minterms })
     };
 
+    onChangeVariable = (e, index) => {
+        const { variables } = this.state;
+        variables[index] = e.target.value.trim();
+        this.setState({ variables });
+    };
+
+    onDeleteVariable = index => {
+        const { variables } = this.state;
+        variables.splice(index, 1);
+        this.setState({ variables });
+    };
+
+    addVariable = () => {
+        const { variables } = this.state;
+        variables.push(variables.length + 1);
+        this.setState({ variables });
+    };
+
+    calculate = () => {
+        let { minterms, variables }= this.state;
+        minterms = minterms.split(',');
+        const max = Math.max(...minterms);
+        const num = Math.ceil(Math.log(max) / Math.log(2));
+        const terms = minterms.map(x => new Term(x, num));
+        const resultFDNF = core.calculateFDNF(terms, variables);
+        const resultSOP = core.calculateSOP(terms, variables);
+
+        this.setState({
+            ...resultFDNF,
+            ...resultSOP,
+        });
+    };
+
     render() {
-        const { minterms } = this.state;
+        const {
+            minterms,
+            variables,
+            FDNFForHuman,
+            FDNFForComputer,
+            SOPForHuman,
+            SOPForComputer,
+            SOPAllForHuman,
+            SOPAllForComputer,
+            SOPBestForHuman,
+            SOPBestForComputer,
+        } = this.state;
 
         return <div styleName="root">
             <Title>Karnaugh Map</Title>
-            <Input value={minterms} onChange={this.onChangeMinterms}/>
-            <Button styleName="root__push">Push me!</Button>
-            <Clipboard>Lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet beatae dicta illum libero natus nobis omnis possimus rem saepe totam. Accusantium architecto blanditiis laudantium maxime natus officiis quasi, vel voluptatem?</Clipboard>
-            {/*<Router history={history}>*/}
-            {/*</Router>*/}
+            <Input value={minterms} onChange={this.onChangeMinterms} placeholder="Minterms"/>
+            <Title size="4">Variables</Title>
+            { variables.map((variableName, index) =>
+                <RowInput
+                    key={index}
+                    value={variableName}
+                    onChange={e => this.onChangeVariable(e, index)}
+                    onDelete={e => this.onDeleteVariable(index)}
+                />
+            ) }
+            <Button styleName="root__push" onClick={this.addVariable}>Add variable</Button>
+            <Button styleName="root__push root__push_bold" onClick={this.calculate}>Push me!</Button>
+
+            { FDNFForHuman && <Title size="4">FDNF for human</Title> }
+            { FDNFForHuman && <Text styleName="root__end-of-block"><InlineMath>{ FDNFForHuman }</InlineMath></Text> }
+
+            { FDNFForComputer && <Title size="4">FDNF for Computer</Title> }
+            { FDNFForComputer && <Clipboard styleName="root__end-of-block">{ FDNFForComputer }</Clipboard> }
+
+            { SOPForHuman && <Title size="4">SOP for human</Title> }
+            { SOPForHuman && <Text styleName="root__end-of-block"><InlineMath>{ SOPForHuman }</InlineMath></Text> }
+
+            { SOPForComputer && <Title size="4">SOP for Computer</Title> }
+            { SOPForComputer && <Clipboard styleName="root__end-of-block">{ SOPForComputer }</Clipboard> }
+
+            { SOPAllForHuman && <Title size="4">SOP for human</Title> }
+            { SOPAllForHuman && <div styleName="root__end-of-block">
+                { SOPAllForHuman.map((value, index) => <Text key={index} >
+                    <InlineMath>{ value }</InlineMath>
+                </Text>) }
+            </div> }
+
+            { SOPAllForComputer && <Title size="4">SOP for Computer</Title> }
+            { SOPAllForComputer && <div styleName="root__end-of-block">
+                { SOPAllForComputer.map((value, index) => <Clipboard key={index} >{ value }</Clipboard>) }
+            </div> }
+
+            { SOPBestForHuman && <Title size="4">BEST SOP for human</Title> }
+            { SOPBestForHuman && <Text styleName="root__end-of-block"><InlineMath>{ SOPBestForHuman }</InlineMath></Text> }
+
+            { SOPBestForComputer && <Title size="4">BEST SOP for Computer</Title> }
+            { SOPBestForComputer && <Clipboard styleName="root__end-of-block">{ SOPBestForComputer }</Clipboard> }
         </div>
     }
 }
